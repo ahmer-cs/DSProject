4a92a02bbd86dd7be552b331cc5fd8d14a25fd9d
Ahmer <ahmerwaqar666@gmail.com>
2023-09-11 01:51:42 +0500

    Sixth Commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,8 @@ import sys
 from src.exception import Custom_Exception
 from src.logger import logging
 import pandas as pd
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -40,5 +42,8 @@ class  DataIngestion:
 
 if __name__== "__main__":
     obj = DataIngestion()
-    obj.initiate_Data_Ingestion()
+    train_data,test_data = obj.initiate_Data_Ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
